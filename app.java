ac17d5c6a292da4d308877326d3c4faf3e0c0a46
SHASHIKUMAR1596 <shashikumarpolisu@gmail.com>
2026-05-17 09:49:19 +0530

    update app.java

--- app.java
+++ app.java
@@ -3,4 +3,13 @@ public class HelloWorld {
         System.out.println("Hello, World!");
     }
 }
-
+//dev2 push from local
+public class AddNumbers {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+        int sum = num1 + num2;
+        
+        System.out.println("The sum is: " + sum);
+    }
+}
